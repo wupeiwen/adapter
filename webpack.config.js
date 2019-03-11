4cbfc38492f143dacfbc507bb2c763e57225690e
wupeiwen <javapeiwen2010@gmail.com>
2019-03-11 09:15:51 +0800

    refactor: 完善webpack配置项

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'adapter.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 };
