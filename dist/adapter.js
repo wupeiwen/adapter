039463b23fa11fccc3f7e3c2890c8493e0a678cc
wupeiwen <javapeiwen2010@gmail.com>
2019-03-13 14:57:46 +0800

    dist: 更新构建

--- dist/adapter.js
+++ dist/adapter.js
@@ -1,1 +1,291 @@
-exports.Adapter=function(n){var t={};function e(r){if(t[r])return t[r].exports;var i=t[r]={i:r,l:!1,exports:{}};return n[r].call(i.exports,i,i.exports,e),i.l=!0,i.exports}return e.m=n,e.c=t,e.d=function(n,t,r){e.o(n,t)||Object.defineProperty(n,t,{enumerable:!0,get:r})},e.r=function(n){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(n,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(n,"__esModule",{value:!0})},e.t=function(n,t){if(1&t&&(n=e(n)),8&t)return n;if(4&t&&"object"==typeof n&&n&&n.__esModule)return n;var r=Object.create(null);if(e.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:n}),2&t&&"string"!=typeof n)for(var i in n)e.d(r,i,function(t){return n[t]}.bind(null,i));return r},e.n=function(n){var t=n&&n.__esModule?function(){return n.default}:function(){return n};return e.d(t,"a",t),t},e.o=function(n,t){return Object.prototype.hasOwnProperty.call(n,t)},e.p="",e(e.s=0)}([function(n,t,e){"use strict";e.r(t);var r=function n(t){return function(n,t){if(!(n instanceof t))throw new TypeError("Cannot call a class as a function")}(this,n),{dataset:t={dimensions:t&&t.dimensions?t.dimensions:["name","value"],source:t&&t.source?t.source:[]}}};var i=function n(t){return function(n,t){if(!(n instanceof t))throw new TypeError("Cannot call a class as a function")}(this,n),{legend:t=t||{}}};var o=function n(t){return function(n,t){if(!(n instanceof t))throw new TypeError("Cannot call a class as a function")}(this,n),{Tooltip:t=t||{}}};var s=function n(t){return function(n,t){if(!(n instanceof t))throw new TypeError("Cannot call a class as a function")}(this,n),{xAxis:t=t||{type:"category"}}};var a=function n(t){return function(n,t){if(!(n instanceof t))throw new TypeError("Cannot call a class as a function")}(this,n),{yAxis:t=t||{}}};var c=function n(t){return function(n,t){if(!(n instanceof t))throw new TypeError("Cannot call a class as a function")}(this,n),{grid:t=t||{}}};var u=function n(t,e){return function(n,t){if(!(n instanceof t))throw new TypeError("Cannot call a class as a function")}(this,n),{series:e=e||[{type:t}]}};e.d(t,"echarts",function(){return f});var f=function n(t,e,f){return function(n,t){if(!(n instanceof t))throw new TypeError("Cannot call a class as a function")}(this,n),this.dataset=new r(e),this.legend=new i(f.legend),this.tooltip=new o(f.tooltip),this.xAxis=new s(f.xAxis),this.yAxis=new a(f.yAxis),this.grid=new c(f.grid),this.series=new u(t,f.series),this.option=Object.assign(this.dataset,this.legend,this.tooltip,this.xAxis,this.yAxis,this.grid,this.series),this.option}}]);
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["Adapter"] = factory();
+	else
+		root["Adapter"] = factory();
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "echarts", function() { return echarts; });
+/* harmony import */ var _dataset__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _legend__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
+/* harmony import */ var _tooltip__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3);
+/* harmony import */ var _xAxis__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(4);
+/* harmony import */ var _yAxis__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(5);
+/* harmony import */ var _grid__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(6);
+/* harmony import */ var _series__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(7);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+
+
+
+
+
+
+
+var echarts = function echarts(type, dataset, option) {
+  _classCallCheck(this, echarts);
+
+  // 数据集
+  this.dataset = new _dataset__WEBPACK_IMPORTED_MODULE_0__["default"](dataset); // 设置图例
+
+  this.legend = new _legend__WEBPACK_IMPORTED_MODULE_1__["default"](option.legend); // 设置提示框
+
+  this.tooltip = new _tooltip__WEBPACK_IMPORTED_MODULE_2__["default"](option.tooltip); // 设置X轴
+
+  this.xAxis = new _xAxis__WEBPACK_IMPORTED_MODULE_3__["default"](option.xAxis); // 设置Y轴
+
+  this.yAxis = new _yAxis__WEBPACK_IMPORTED_MODULE_4__["default"](option.yAxis); // 设置网格
+
+  this.grid = new _grid__WEBPACK_IMPORTED_MODULE_5__["default"](option.grid); // 设置系列
+
+  this.series = new _series__WEBPACK_IMPORTED_MODULE_6__["default"](type, option.series); // 组合配置项
+
+  this.option = Object.assign(this.dataset, this.legend, this.tooltip, this.xAxis, this.yAxis, this.grid, this.series);
+  return this.option;
+};
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Dataset; });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Dataset = function Dataset(dataset) {
+  _classCallCheck(this, Dataset);
+
+  dataset = {
+    dimensions: dataset && dataset.dimensions ? dataset.dimensions : ['name', 'value'],
+    source: dataset && dataset.source ? dataset.source : []
+  };
+  return {
+    'dataset': dataset
+  };
+};
+
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Legend; });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Legend = function Legend(legend) {
+  _classCallCheck(this, Legend);
+
+  legend = legend ? legend : {};
+  return {
+    'legend': legend
+  };
+};
+
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Tooltip; });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Tooltip = function Tooltip(_Tooltip) {
+  _classCallCheck(this, Tooltip);
+
+  _Tooltip = _Tooltip ? _Tooltip : {};
+  return {
+    'Tooltip': _Tooltip
+  };
+};
+
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return XAxis; });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var XAxis = function XAxis(axis) {
+  _classCallCheck(this, XAxis);
+
+  axis = axis ? axis : {
+    type: 'category'
+  };
+  return {
+    xAxis: axis
+  };
+};
+
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return YAxis; });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var YAxis = function YAxis(axis) {
+  _classCallCheck(this, YAxis);
+
+  axis = axis ? axis : {};
+  return {
+    yAxis: axis
+  };
+};
+
+
+
+/***/ }),
+/* 6 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Grid; });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Grid = function Grid(grid) {
+  _classCallCheck(this, Grid);
+
+  grid = grid ? grid : {};
+  return {
+    grid: grid
+  };
+};
+
+
+
+/***/ }),
+/* 7 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Series; });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Series = function Series(type, series) {
+  _classCallCheck(this, Series);
+
+  series = series ? series : [{
+    type: type
+  }];
+  return {
+    series: series
+  };
+};
+
+
+
+/***/ })
+/******/ ]);
+});
